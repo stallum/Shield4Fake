50614b47f83fa93299b3e1d0aa821e04ff855ef4
João Felipe Fiari Tigre <joaofelipetigre@gmail.com>
2025-05-22 11:41:17 -0300

    aplicação de pagina web

--- app/app.py
+++ app/app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request
 from scripts.modelo import classificarNoticia
+from langdetect import detect
+from deep_translator import GoogleTranslator
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     resultado = None
+    noticia_traduzida = None
+    
     if request.method == 'POST':
         noticia = request.form['noticia']
+        idioma = detect(noticia)
+        if idioma != 'en':
+            noticia_traduzida = GoogleTranslator(source='auto', target='en').translate(noticia)
+        else: 
+            noticia_traduzida = noticia 
+                   
         label, confianca = classificarNoticia(noticia)
         resultado = {
             # Classificação
