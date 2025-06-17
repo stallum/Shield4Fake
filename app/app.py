a1b90f1371ca2db504fed685466d32df332ae775
Artur Gabriel "stallum <stallumgp8@gmail.com>
2025-06-17 12:04:45 -0300

    adição da função de verificação por  link

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from scripts.modelo import classificarNoticia
+from scripts.scrapy import extrairTexto
+
 app = Flask(__name__)
 
 # rota de home do projeto
@@ -8,14 +10,19 @@ def index():
     resultado = None
     
     if request.method == 'POST':
-        noticia = request.form['noticia']
-        label = classificarNoticia(noticia)
+        if 'noticia' in request.form:
+            noticia = request.form['noticia']
+        else:
+            if 'link' in request.form:
+                url = request.form['link']
+                noticia = extrairTexto(url)
+        
+        label = classificarNoticia(str(noticia))
         resultado = {
             # Classificação
             'label': f'🔎{label}'
         }
     return render_template('teste.html', resultado=resultado)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
